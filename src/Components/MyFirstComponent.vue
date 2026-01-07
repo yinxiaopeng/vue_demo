cd67e8a54602e6ea4d267e70e82995c75addc753
yinxiaopeng <928021376@qq.com>
2026-01-07 19:34:37 +0800

    Create MyFirstComponent.vue with template and styles

---
+++ src/Components/MyFirstComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="custom-component">
+    <h3>{{ subTitle }}</h3>
+    <p>这是我创建的第一个自定义 Vue 组件！</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// 子组件的响应式数据
+const subTitle = ref('自定义组件标题')
+</script>
+
+<style scoped>
+.custom-component {
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 6px;
+}
+</style>
